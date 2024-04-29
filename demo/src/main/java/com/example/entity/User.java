b775edd9b4bc347f07a001a5a639428c31d33e5b
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-29 10:23:15 +0200

    define User model

--- demo/src/main/java/com/example/entity/User.java
+++ demo/src/main/java/com/example/entity/User.java
@@ -1,4 +1,26 @@
 package com.example.entity;
 
+import com.example.entity.enums.ERole;
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User  {
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String bio;
+    private String password;
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime createdDate;
+    @PrePersist
+    protected  void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
