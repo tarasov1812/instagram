d71a59bbd457b8b1f76d4907724bb61c5550b9f2
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-29 10:35:29 +0200

    define Comment model

--- demo/src/main/java/com/example/entity/Comment.java
+++ demo/src/main/java/com/example/entity/Comment.java
@@ -1,4 +1,18 @@
 package com.example.entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String message;
+    private LocalDateTime createdDate;
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
