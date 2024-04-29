96022370119a8ccb7dd368518d3b5cd40ffb4e00
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-29 10:28:30 +0200

    define Post model

--- demo/src/main/java/com/example/entity/Post.java
+++ demo/src/main/java/com/example/entity/Post.java
@@ -1,4 +1,25 @@
 package com.example.entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Post {
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+    private Set<String> likedUsers = new HashSet<>();
+    private User user;
+    private List<Comment> comments = new ArrayList<>();
+    private LocalDateTime createdDate;
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
