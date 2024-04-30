56e4d4a34adba0a6788b304cb9e4406ee837c9fa
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-30 16:34:47 +0200

    create secutiry config

---
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo.security;
+
+import com.example.demo.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        proxyTargetClass = true
+)
+public class SecurityConfig {
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests((authz) -> authz
+                .requestMatchers(SecurityConstants.SIGN_UP_URLS).permitAll()
+                .anyRequest().authenticated()
+        );
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+
+}
--- demo/src/main/java/com/example/demo/security/SecurityConstants.java
+++ demo/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.example.demo.security;
 
 public class SecurityConstants {
-    public static final String SING_UP_URLS = "/api/auth/**";
+    public static final String SIGN_UP_URLS = "/api/auth/**";
     public static final String SECRET = "SecretKeyGenJWT";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
