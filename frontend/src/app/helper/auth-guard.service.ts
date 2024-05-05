bff464903892bacbb653e91e97b0fd77f63ffc81
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-05-05 14:51:37 +0200

    modify auth guard service

--- frontend/src/app/helper/auth-guard.service.ts
+++ frontend/src/app/helper/auth-guard.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, GuardResult, MaybeAsync, RouterStateSnapshot} from "@angular/router";
+import {TokenStorageService} from "../service/token-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router,
+              private tokenService: TokenStorageService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    const currentUser = this.tokenService.getUser();
+    if (currentUser) {
+        return true;
+    }
+
+    this.router.navigate(['/signup'], {queryParams: {returnUrl: state.url}});
+    return false;
+  }
 }
