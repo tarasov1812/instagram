35df3ff0d47b2939fde97e11ea410d350fa4a844
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-05-19 15:22:42 +0200

    create edit user component

--- frontend/src/app/user/edit-user/edit-user.component.ts
+++ frontend/src/app/user/edit-user/edit-user.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {NotificationService} from "../../service/notification.service";
+import {UserService} from "../../service/user.service";
+import {User} from "../../models/User";
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.css'
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
+  // @ts-ignore
+  public profileEditForm: FormGroup;
+
+  constructor(private dialogRef: MatDialogRef<EditUserComponent>,
+              private fb: FormBuilder,
+              private notificationService: NotificationService,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.profileEditForm = this.createProfileForm();
+  }
+
+  createProfileForm(): FormGroup {
+    return this.fb.group({
+      firstName: [
+        this.data.user.firstname,
+        Validators.compose([Validators.required])
+      ],
+      lastName: [
+        this.data.user.lastname,
+        Validators.compose([Validators.required])
+      ],
+      bio: [
+        this.data.user.bio,
+        Validators.compose([Validators.required])
+      ]
+    })
+  }
+
+  submit(): void {
+    this.userService.updateUser(this.updateUser())
+      .subscribe(() => {
+        this.notificationService.showSnackBar('User updated successfully');
+        this.dialogRef.close();
+      })
+  }
+
+  private updateUser(): User {
+    this.data.user.firstname = this.profileEditForm.value.firstName;
+    this.data.user.lastname = this.profileEditForm.value.lastName;
+    this.data.user.bio = this.profileEditForm.value.bio;
+    return this.data.user;
+  }
+
+  closeDialog(){
+    this.dialogRef.close();
+  }
 
 }
